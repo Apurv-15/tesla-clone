53ef02d3c19f3221e70200920d90428cb6601241
Apurv-15 <95355502+Apurv-15@users.noreply.github.com>
2023-06-11 18:15:28 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Section from "./components/Section";
 
 export default function App() {
   return (
     <div>
      <Header/>  
      <Home/>
+     <h1>hi </h1>
+     <Section/>
      </div>
   );
 }
---
+++ src/components/Section.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-"
+
+
+export default function Section() {
+  return (
+    <div>
+    <Wrap>
+        <ItemText>
+            <h1>Model S</h1>
+             <p>Order online</p>
+             
+          </ItemText>
+      </Wrap>
+
+     </div>
+  );
+}
