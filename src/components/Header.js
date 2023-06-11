d0807ac045ba43cfb9a2ac5d8dddf140beec5d33
Apurv-15 <95355502+Apurv-15@users.noreply.github.com>
2023-06-11 15:04:53 +0530

    1 file modified, 1 added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
-import "./style.css";
-import Header from "./components/Header"
+
 
 export default function App() {
   return (
     <div>
-     <Header/>
+     header
      </div>
   );
 }
