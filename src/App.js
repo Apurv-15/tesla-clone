049eff604e773da7e36a043fc7b41e5feb8337cd
Apurv-15 <95355502+Apurv-15@users.noreply.github.com>
2023-06-11 18:30:21 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
-// import Header from "./components/Header.js";
+import Header from "./components/Header.js";
 // import Home from "./components/Home.js";
 // import Section from "./components/Section.js";
 
 export default function App() {
   return (
     <div>
-     {/* <Header/>  
-     <Home/> */}
+     <Header/>  
+     <Home/>
      <h1>hi </h1>
      {/* <Section/> */}
      </div>
